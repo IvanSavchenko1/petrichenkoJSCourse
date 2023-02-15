e8f677747a2e1bac2c88d79fcf11ed0b860a0633
Ivan Savchenko <ivan.savchenko@gmail.com>
2023-02-15 21:51:59 +0200

    Closures

---
+++ Coding Exercise 15/script.js
@@ -0,0 +1,68 @@
+/*
+Задание:
+
+  У вас есть объект с данными о ресторане.
+  Начинающий разработчик создал несколько функций,
+  которые работают неправильно и он не может понять почему.
+  Нужно исправить функции так, чтобы они давали всегда правильный результат.
+
+1) Функция isOpen не хочет правильно работать.
+Что мы уже не пробовали подставлять в неё - результат все время неправильный.
+Необходимо найти причины и исправить.
+
+ */
+
+const restorantData = {
+  menu: [
+    {
+      name: 'Salad Caesar',
+      price: '14$'
+    },
+    {
+      name: 'Pizza Diavola',
+      price: '9$'
+    },
+    {
+      name: 'Beefsteak',
+      price: '17$'
+    },
+    {
+      name: 'Napoleon',
+      price: '7$'
+    }
+  ],
+  waitors: [
+    {name: 'Alice', age: 22}, {name: 'John', age: 24}
+  ],
+  averageLunchPrice: '20$',
+  openNow: true
+};
+
+function isOpen(prop) {
+  let answer = '';
+  prop ? answer = 'Открыто' : answer = 'Закрыто';
+
+  return answer;
+}
+
+console.log(isOpen(restorantData.openNow))
+
+function isAverageLunchPriceTrue(fDish, sDish, average) {
+  if ((+fDish.price.slice(0, -1)) + (+sDish.price.slice(0, -1)) < +average.slice(0, -1)) {
+    return 'Цена ниже средней';
+  } else {
+    return 'Цена выше средней';
+  }
+}
+
+console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
+
+function transferWaitors(data) {
+  const copy = Object.assign({}, data);
+
+  copy.waitors = [{name: 'Mike', age: 32}];
+  return copy;
+}
+
+console.log(transferWaitors(restorantData))
+console.log(restorantData)
