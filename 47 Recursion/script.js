d4360a79033e94534fa10314a13f7c62709eeb7e
Ivan Savchenko <ivan.savchenko@gmail.com>
2023-03-06 23:19:34 +0200

    47 Recursion

---
+++ 47 Recursion/script.js
@@ -0,0 +1,95 @@
+'use strict';
+
+function pow(x, n) {
+  if (n === 1) {
+    return x;
+  } else {
+    return x * pow(x, n-1);
+  }
+ }
+ console.log( pow(2,3 ))
+
+
+
+let students = {
+  js:[{
+    name: 'John',
+    progress: 100
+  }, {
+    name: 'Ivan',
+    progress: 60
+  }],
+  html: {
+    basic:[{
+      name: 'Peter',
+      progress: 20
+    }, {
+      name: 'Ann',
+      progress: 18
+    }],
+    pro: [{
+      name: 'Sam',
+      progress: 10
+    }],
+//добавлен обьект, который ломает код getTotalProgressByIteration функции, но рекурсия работает
+    semi: {
+      students:[{
+        name:'Test',
+        progress: 100
+      }]
+    }
+  }
+};
+
+function getTotalProgressByIteration(data) {
+   let total = 0;
+   let students = 0;
+// используется Object values
+
+  for (let course of Object.values(data)) {
+    // typeof не сработает, так как вернет все как обьект поэтому Array.isArray
+
+    if (Array.isArray(course)) {
+      students +=course.length;
+
+      for (let i=0; i < course.length; i++)
+        total += course[i].progress;
+    } else {
+      for (let subCourse of Object.values(course)) {
+        students +=subCourse.length;
+
+        for (let i=0; i < subCourse.length; i++) {
+          total += subCourse[i].progress;
+        }
+      }
+    }
+  }
+
+   return total/students;
+}
+
+console.log(getTotalProgressByIteration(students))
+
+
+function getTotalProgressByRecursion(data) {
+  if (Array.isArray(data)) {
+    let total = 0;
+
+    for (let i=0; i < data.length; i++)
+      total += data[i].progress;
+
+    return [total, data.length]
+  } else {
+    let total = [0,0];
+
+    for(let subData of Object.values(data)) {
+      const subDataArr = getTotalProgressByRecursion(subData);
+      total[0] +=subDataArr[0];
+      total[1] +=subDataArr[1];
+    }
+    return total;
+  }
+}
+
+const result = getTotalProgressByRecursion(students);
+console.log(result[0]/result[1]);
